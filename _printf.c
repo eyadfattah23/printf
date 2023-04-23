3eef8da802116f3c03f5c7f4406893292df1ee2a
Abdulrahman-Alpha <sir.abdulrahman201@gmail.com>
2023-04-23 14:52:23 +0200

    update

--- _printf.c
+++ _printf.c
@@ -3,20 +3,19 @@
 /**
  * _strlen - returns the length of a string.
  * @s: string to be measured
- * Return: length of string
+ * Return: len of string;
  */
+
 int _strlen(char *s)
 {
-    int i = 0;
+	int i = 0;
 
-    while (*(s + i) != '\0')
-    {
-        i++;
-    }
-
-    return (i);
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
-
 /**
  * _printf - produces output according to a format.
  * @format: character string
@@ -25,59 +24,63 @@ int _strlen(char *s)
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    unsigned int i, count = 0;
-    char *str_to_print;
+	va_list args;
+	unsigned int i, count = 0;
+	char *str_to_print;
 
-    if (format == NULL)
-    {
-        return (0);
-    }
-
-    va_start(args, format);
-
-    for (i = 0; format[i] != '\0'; i++)
-    {
-        if (format[i] == '%')
-        {
-            i++;
-
-            switch (format[i])
-            {
-                case 'c':
-                    _putchar((char)va_arg(args, int));
-                    count++;
-                    break;
-                case 's':
-                    str_to_print = va_arg(args, char *);
-                    _puts(str_to_print);
-                    count += _strlen(str_to_print);
-                    break;
-                case '%':
-                    _putchar('%');
-                    count++;
-                    break;
-                case 'd':
-                case 'i':
-                    printf("%d", va_arg(args, int));
-                    count++;
-                    break;
-                default:
-                    /* handling unknown specifiers */
-                    _putchar('%');
-                    _putchar(format[i]);
-                    count += 2;
-                    break;
-            }
-        }
-        else
-        {
-            _putchar(format[i]);
-            count++;
-        }
-    }
-
-    va_end(args);
-
-    return (count);
+	if (format == NULL)
+	{
+		return (0);
+	}
+	va_start(args, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+				{
+					_putchar((char) va_arg(args, int));
+					count++;
+					break;
+				}
+				case 's':
+				{
+					str_to_print = va_arg(args, char *);
+					if (str_to_print == NULL)
+					{
+						_puts("(null)");
+						count += 5;
+					} else
+					{
+					_puts(str_to_print);
+					count += _strlen(str_to_print);
+					}
+					break;
+				}
+				case '%':
+				{
+					_putchar('%');
+					count++;
+					break;
+				}
+				default:
+				{
+					/*handling unknown specifiers*/
+					_putchar('%');
+					_putchar(format[i]);
+					count += 2;
+					break;
+				}
+			}
+		} else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+	}
+	va_end(args);
+	return (count);
 }
