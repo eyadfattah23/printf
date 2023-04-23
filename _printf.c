f5efbe401538e8c8a1c751c862f2fcf404389cee
Abdulrahman-Alpha <sir.abdulrahman201@gmail.com>
2023-04-23 11:55:33 +0200

    added d, i cases

--- _printf.c
+++ _printf.c
@@ -66,6 +66,14 @@ int _printf(const char *format, ...)
 					count++;
 					break;
 				}
+				case 'd':
+                case 'i':
+                    {
+                        int num = va_arg(args, int);
+                        printf("%d", num);
+                        count++;
+                        break;
+                    }
 				default:
 				{
 					/*handling unknown specifiers*/
