b5074fa59412cde3dd76f89928e8ff329777bbf8
Abdulrahman-Alpha <sir.abdulrahman201@gmail.com>
2023-04-24 20:43:05 +0200

    Updated d, i cases

--- _printf.c
+++ _printf.c
@@ -5,6 +5,25 @@
  * @s: string to be measured
  * Return: len of string;
  */
+int print_number(int n)
+{
+        int count = 0;
+
+        if (n < 0)
+        {
+                count += _putchar('-');
+                n = -n;
+        }
+
+        if (n / 10)
+        {
+                count += print_number(n / 10);
+        }
+
+        count += _putchar(n % 10 + '0');
+
+        return (count);
+}
 
 int _strlen(char *s)
 {
