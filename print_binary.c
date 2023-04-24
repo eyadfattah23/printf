f7c60b44d1281e401c21dcfe1bf10a57cd0b9f57
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-04-24 22:30:34 +0200

    betty

--- print_binary.c
+++ print_binary.c
@@ -1,19 +1,20 @@
 #include "main.h"
 /**
- * print_number - prints an integer
+ * print_binary - prints an integer
  * @n: integer to be printed
+ * Return: number of binary digits
  */
 int print_binary(unsigned int n)
 {
 	int count = 0;
 
-	if (n / 2) 
-	{ 
-	count += print_binary(n / 2); 
-	} 
+	if (n / 2)
+	{
+	count += print_binary(n / 2);
+	}
 
-	count++; 
-	_putchar(n % 2 + '0'); 
+	count++;
+	_putchar(n % 2 + '0');
 
-	return count; 
+	return (count);
 }
