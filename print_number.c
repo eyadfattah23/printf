3d9c861f82ec8dab14d7e9cd887e07c18dcf8e45
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-04-24 22:11:57 +0200

    betty update

--- print_number.c
+++ print_number.c
@@ -2,28 +2,29 @@
 /**
  * print_number - prints an integer
  * @n: integer to be printed
+ * Return: number of digits
  */
 int print_number(int n)
 {
-        int count = 0;
+		int count = 0;
 
-        if (n < 0)
-        {
-                count += _putchar('-');
-                n = -n;
-        }
+		if (n < 0)
+		{
+			count += _putchar('-');
+			n = -n;
+		}
 
-        if (n / 10)
-        {
-                count += print_number(n / 10);
-        }
+		if (n / 10)
+		{
+			count += print_number(n / 10);
+		}
 
-        count += _putchar(n % 10 + '0');
+		count += _putchar(n % 10 + '0');
 
-        if (n == 0)
-        {
-                return (1);
-        }
+		if (n == 0)
+		{
+			return (1);
+		}
 
-        return (count);
+		return (count);
 }
