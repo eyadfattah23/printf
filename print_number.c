bc8bb9e99645169fe4bfa89a117dffa060fd3759
Abdulrahman-Alpha <sir.abdulrahman201@gmail.com>
2023-04-24 21:13:04 +0200

    Updated d, i cases

--- print_number.c
+++ print_number.c
@@ -2,24 +2,28 @@
 /**
  * print_number - prints an integer
  * @n: integer to be printed
-*/
+ */
 int print_number(int n)
 {
-	int count = 0;
+        int count = 0;
 
-	if (n < 0) 
-	{ 
-	count++; 
-	n = -n; 
-	} 
+        if (n < 0)
+        {
+                count += _putchar('-');
+                n = -n;
+        }
 
-	if (n > 0) 
-	{ 
-	count += print_number(n / 10); 
-	}
+        if (n / 10)
+        {
+                count += print_number(n / 10);
+        }
 
-	count++; 
-	_putchar(n % 10 + '0'); 
+        count += _putchar(n % 10 + '0');
 
-	return count;
+        if (n == 0)
+        {
+                return (1);
+        }
+
+        return (count);
 }
